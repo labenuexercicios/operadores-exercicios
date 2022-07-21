289d169db77e823ba3ffa77e05f0409c15bb0a8b
Eduardo Fernandes <eduardo.eduesf@gmail.com>
2022-07-21 18:09:00 -0300

    Ajustando o Exe 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -7,8 +7,8 @@ console.log("O primeiro número é maior que o segundo?",resp1)
 let resp2 = num1 === num2
 console.log("O primeiro número é igual que o segundo?",resp2)
 
-let resp3 = num1 % num2
+let resp3 = num1 % num2 === 0
 console.log("O primeiro número é divisível que o segundo?",resp3)
 
-let resp4 = num2 % num1
+let resp4 = num2 % num1 === 0
 console.log("O segundo número é divisível que o primeiro?",resp4)
