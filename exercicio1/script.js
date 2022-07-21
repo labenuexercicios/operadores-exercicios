8a376ab82fd469b8855c4cbc66d724e0ef5d6d5f
falcaomm <manu.m.falcao@gmail.com>
2022-07-21 18:28:35 -0300

    exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let numero = Number (prompt("Escreva aqui um número par:"))
+
+console.log("Esse é seu número", numero);
+console.log(typeof numero);
+
+numero = numero/2
+console.log("Esse é seu número divido por 2", numero);
+console.log(typeof numero);
+
