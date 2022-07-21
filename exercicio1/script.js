46b7263c21cc16e6c396018d78b108a2af23bc2a
Arthur Prado <arthur.matheus.prado@gmail.com>
2022-07-20 23:17:47 -0300

    exercicio-aula08

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+//  Exercício 1
+
+// A-)
+let numeroPar = prompt ("Insira aqui um Número Par")
+
+// B-)
+console.log(numeroPar % 2)
+
+// C-) O padrão criado foi. Toda vez que um número par é inserido o resultado é igual a zero. Porque todo Número par, quando dividido por 2 fará divisões iguais.
+
+// D-) O padrão de sobra zero foi alterado quando o usuário inserir um número impar. Porque a divisão por dois gerará uma sobra.
---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+let numero1 = prompt("Adicione um número aqui.")
+let numero2 = prompt("Adicione mais um número aqui.")
+
+let resposta1 = numero1
+let resposta2 = numero2
+
+console.log("Primeiro numero adicionado:", resposta1)
+console.log("Segundo numero adicionado:", resposta2)
+console.log("O primeiro numero é maior que o segundo?", numero1>numero2)
+console.log("O primeiro numero é igual ao segundo?", numero1===numero2)
+console.log("O primeiro numero é divisível pelo segundo?", numero1%numero2===0)
+console.log("O segundo numero é divisível pelo primeiro?", numero1%numero2===0)
+
+// Quando o numero1 e o numero2 forem divisíveis entre si. A sobra sempre será 0
