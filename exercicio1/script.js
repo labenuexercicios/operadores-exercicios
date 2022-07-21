fff2c68a765b3beaeeb39f096346378b6372e1e4
marcusvrr <marcusvrr2@gmail.com>
2022-07-20 21:56:08 -0300

    entrega exer1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let numPar = Number(prompt("Insira um numero par"))
+let restodaDiv = numPar % 2
+
+console.log (restodaDiv)
+//o padrão de respostas é sempre 0 sendo q o resto da divisão de numero par e zero
+
+//se um numero impar o resto da divisão da um numero maior q zero
