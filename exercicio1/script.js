f5bd48e82ee69cb5a5007ab72885ad36a7cb9d06
victor <jv.paixao.762.g@gmail.com>
2022-07-21 16:08:11 -0300

    modificação2

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+// Construa um programa que:
+
+// -a) Peça ao usuário que insira um número par
+prompt('insira um numero par')
+const a = ('insira um numero par')
+ let b = 2 
+  const resultadoDaDivisão = a / 2
+  console.log(resultadoDaDivisão)
+
+
+//  let Number = prompt('insira um numero par')
+//   let a = Number
+//   let b = 2 
+
+//   console.log(b / 2)
+
+// b) Imprima no console o resto da divisão desse número por 2.
+
+  console.log(typeof b % 2)
+
+// c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+
+// d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código
+
+// 💡 Dica: não se esqueça de converter as respostas para o tipo número
+
