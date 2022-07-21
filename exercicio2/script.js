73d0a0fdee30803549011eaae8b98e08e86ab2a4
falcaomm <manu.m.falcao@gmail.com>
2022-07-21 20:25:47 -0300

    Atualizações aula feedback

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -5,6 +5,9 @@ console.log("Esse é seu primeiro número =", numero1);
 console.log("Esse é seu segundo número =", numero2);
 
 console.log("O primeiro numero é maior que segundo?", numero1 > numero2);
+
 console.log("O primeiro numero é igual ao segundo?", numero1 === numero2);
+
 console.log("O primeiro numero é divisível pelo segundo?", numero1 % numero2 === 0);
+
 console.log("O segundo numero é divisível pelo primeiro?", numero2 % numero1 === 0);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -6,6 +6,8 @@ console.log("5 é igual a 5 ou é igual a “5” =", operação2);
 
 let operação3 = !20>50
 console.log("Negação de vinte é maior que cinquenta", operação3);
+//quando é uma expressão (não apenas um número sozinho) sem parênteses o js ignora o !
 
 let operação4 = !(20>50)
 console.log("Negação de (vinte é maior que cinquenta)", operação4);
+//negação de algo falso vira verdadeira
