390a5cf1977de88229ae0b89088887f1210f489d
Thaila Marani <thaila_marani@hotmail.com>
2022-07-21 18:31:20 -0300

    Conclusão dos exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+//EXERCÍCIO 1
+
+let numeroPar = prompt ('Digite um número par');
+console.log('Número Par', numeroPar);
+numeroPar = numeroPar%2
+console.log('Resto', numeroPar)
+//Por se tratar de um número par, *o resto da divisão* sempre vai ser 0.
+//Ao inserir um número ímpar, *o resto da divisão* é sempre 1.
