0c8350aee893e3d2528bb54719e2396d3f3952ba
JulianDiel <drdiel@hotmail.com>
2022-07-20 22:30:16 -0300

    exercicios concluidos

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let par1 = Number(prompt(`Digite um numero par`));
+let restDiv1 = par1 /2;
+let restDiv2 = restDiv1 % 2 ==0 ;
+
+console.log(restDiv1 + restDiv2);
+
+// como está /2, ao colocar exemplo 44, ele da como se 2 multiplica 23x e como resultado da numero inteiro;
+// ao coloca numero impar da numero quebrado exemplo 33, o resultado da como 16,5
