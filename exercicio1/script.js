7fa8f2577e13ac160a8f1ae1c661570e7fa25d03
danilogmarques <danilogustavomarques@gmail.com>
2022-07-21 15:43:22 -0300

    resoluçãoExercicíos

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const numeroPar = prompt("insira um número par")
+
+const respotaUsuario = numeroPar % 2 
+
+console.log(respotaUsuario)
+
+//notei um padrão de que com os número pares o resto sempre a zero e que caso 
+// o usúario responda com o número ímpar temos diversos respostas com resto.
+
---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+const pergunta1 = prompt("digite um número")
+const pergunta2 = prompt("digite um número")
+
+
+
+console.log("O primeiro numero é maior que segundo?" , pergunta1 > pergunta2)
+console.log("O primeiro numero é igual ao segundo?" , pergunta1 === pergunta2)
+console.log("O primeiro numero é divisível pelo segundo?" , pergunta1 / pergunta2)
+console.log("O segundo numero é divisível pelo primeiro?" , pergunta1 / pergunta2)
