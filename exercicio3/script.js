95a81239d30ef1767b6caa35ba2eb9c6f72fc1eb
falcaomm <manu.m.falcao@gmail.com>
2022-07-21 18:51:12 -0300

    exercício 3

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+let operação1 = 5>20 && 5<2
+console.log("5 é maior que 20 e também é menor que 2 =", operação1);
+
+let operação2 = 5===5 || 5==="5"
+console.log("5 é igual a 5 ou é igual a “5” =", operação2);
+
+let operação3 = !20>50
+console.log("Negação de vinte é maior que cinquenta", operação3);
+
+let operação4 = !(20>50)
+console.log("Negação de (vinte é maior que cinquenta)", operação4);
