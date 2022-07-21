617be86341e44e48f52f375dd6c436adc04b7c5b
devfelixarthur <de.felixarthur@gmail.com>
2022-07-21 20:11:50 -0300

    Exercicios Operadores

---
+++ projeto/exercicio1/script.js
@@ -0,0 +1,11 @@
+let insiraUmNumero = +prompt ('Insira um número par')
+let denominador = 2
+let resto = insiraUmNumero % denominador
+
+console.log (
+    resto
+)
+
+// Sim, sempre que uso numeros ímpares o resto é igual a 1
+// Porém quando uso casa decimal o resto me é dado com casa decimal também.
+
---
+++ projeto/exercicio3/script.js
@@ -0,0 +1,10 @@
+const oper1 = 5>20 && 5<2
+const oper2 = 5 === 5 || 5 === "5"
+const oper3 = 20>50
+const oper4 = !(20>50)
+
+console.log (`5 é maior que 20 e também é menor que 2 ${oper1}  
+5 é igual a 5 ou é igual a “5” ${oper2} 
+negação de vinte é maior que cinquenta ${oper3} 
+negação de (vinte é maior que cinquenta) ${oper4}`
+)
