bd4bc28cb548098da347e2ad1fc806aed626c961
mavfc <mavfc46@gmail.com>
2022-07-20 23:57:27 -0300

    008-operadores-mvfc

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let npar = prompt('Insira um n° PAR')
+let r = npar%2
+console.log(r)
+
+/* c) notei. todos os numeros pares são divisíveis pelo n° 2, então sempre restará 0
+na divisão. */
+
+/* d) sempre irá sobrar o n° 1 no resto, por padrão. O que é
+bem interessante.*/
