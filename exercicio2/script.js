c0a2a5cfe39a606938f9a2a6662d133f439c69bb
Stefanie Duwe <fafidw@gmail.com>
2022-07-20 22:06:24 -0300

    Exercício concluído

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+const numero1 = prompt('Insira um número')
+const numero2 = prompt('Insira outro número')
+
+console.log(numero1, numero2)
+
+const primeiroNumeroEmaiorQueSegundo = numero1 > numero2
+const primeiroNumeroEIgualAoSegundo = numero1 === numero2
+const PrimeiroNumeroEDivisivelPeloSegundo = numero1 / numero2
+const segundoNumeroEDivisivelPeloPrimeiro = numero2 / numero1
+
+console.log(primeiroNumeroEmaiorQueSegundo)
+console.log(primeiroNumeroEIgualAoSegundo)
+console.log(PrimeiroNumeroEDivisivelPeloSegundo)
+console.log(segundoNumeroEDivisivelPeloPrimeiro)
---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+const operacaoA = 5 > 20 && 5 < 2
+const operacaoB = 5 === 5 || 5 === "5"
+const operacaoC = !20 > 50
+const operacaoD = !(20 > 50)
+
+console.log(operacaoA)  //false
+console.log(operacaoB)  //true
+console.log(operacaoC)  //false
+console.log(operacaoD)  //true
