e7ba7633e50c74861b4dd86af1af879b16539f8c
Kieffer Torricilia <kieffer.torricilia@gmail.com>
2022-07-21 18:23:14 -0300

    Exercício 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let numero = Number(prompt("digite um número par"))
+
+let resto = numero % 2
+
+console.log(resto)
+
+//O padrão é que o resto da divisão sempre é zero
+
+/*Se for inserido um número ímpar o resto será o que sobrou da divisão, que não era possível
+fazer com número inteiros*/
