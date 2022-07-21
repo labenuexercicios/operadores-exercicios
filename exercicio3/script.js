42868acfa9bccc6d8a5d62c0ffe3a075e40450c0
Elenilson <elenilsonsantos9@gmail.com>
2022-07-21 18:36:30 -0300

    exercicio atualizado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,10 +1,10 @@
-const operaçãoA = 5 > 20 < 2
+const operaçãoA = 5 > 20 && 20 < 2
 console.log ( operaçãoA	)
 
-const operaçãoB = 5 === 5 === "5"
+const operaçãoB = 5 === 5 || 5 === "5"
 console.log (operaçãoB)
 
-const operaçãoC = !20 > 5
+const operaçãoC = !20 > 50
 console.log (operaçãoC)
 
 const operaçãoD = !(20 > 50)
