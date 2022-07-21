a559d6928f558190459fcf9e8b966eed8d292018
Elenilson <elenilsonsantos9@gmail.com>
2022-07-21 18:33:07 -0300

    exercicios feitos

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+const operaçãoA = 5 > 20 < 2
+console.log ( operaçãoA	)
+
+const operaçãoB = 5 === 5 === "5"
+console.log (operaçãoB)
+
+const operaçãoC = !20 > 5
+console.log (operaçãoC)
+
+const operaçãoD = !(20 > 50)
+cosole.log (operaçãoD)
