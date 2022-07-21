6d910ea034f19aaf64d7f47bb7043bc68f7aefcf
Sirius Rigel <anagsp22@gmail.com>
2022-07-20 23:05:08 -0300

    git commit

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let par = prompt ("Insira um número par")
+console.log("O resto da divisão é", Number(par%=2));
+
+// Números pares divididos por dois tem sempre resto = 0
+// Ele vai dividir o número ímpar por 2 é o que sobra ele vai mostrar na tela
---
+++ exercicio2/script.js
@@ -0,0 +1,6 @@
+let n1 = prompt("Digite um número")
+let n2 = prompt("Digite outro número")
+console.log("O primeiro numero é maior que segundo?",n1>n2);
+console.log("O primeiro numero é igual ao segundo?", n1===n2);
+console.log("O primeiro numero é divisível pelo segundo?", n1%n2==0);
+console.log("O segundo numero é divisível pelo primeiro?", n2%n1==0);
