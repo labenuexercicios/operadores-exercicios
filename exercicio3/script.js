636aa6945b549c316fcc4ccd673ae99b96dfbccf
angelicagalvao <angelicagalvao18@gmail.com>
2022-07-21 17:43:57 -0300

    A008 operadores

---
+++ exercicio3/script.js
@@ -0,0 +1,16 @@
+/* a) 5 é maior que 20 e também é menor que 2;
+
+b) 5 é igual a 5 ou é igual a “5”;
+
+c) negação de vinte é maior que cinquenta
+
+d) negação de (vinte é maior que cinquenta) */
+
+let operacao1 = (5 > 20 && 5 < 2)
+let operacao2 = (5 == 5 || 5 == "5")
+let operacao3 = (-20 > 50)
+let operacao4 = (-20 > 50)
+console.log(operacao1)
+console.log(operacao2)
+console.log(operacao3)
+console.log(operacao4)
