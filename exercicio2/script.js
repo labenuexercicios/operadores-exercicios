d73fd4676fdb02faeb8a9ef2a48610fb8d069382
Lucas Caffeu <lucas_caffeu_psico@hotmail.com>
2022-07-22 20:29:06 -0300

    exercicios-aula-008

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let numero1 = +prompt('digite um numero')
+let numero2 = +prompt('digite outro numero')
+
+console.log(numero1 >= numero2)
+console.log(numero1 === numero2)
+console.log(numero1 % numero2 === 0)
+console.log(numero2 % numero1 === 0)
---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+let A = 5
+console.log(A > 20 && A < 2)
+// Devolve False  
+console.log(A === 5 || A === '5')
+// Devolve true
+let B = !20
+console.log(B > 50)
+console.log(!(20 > 50))
