dff8e4f2487d5a2fb8e496fabf8c67fd888ac744
angelicagalvao <angelicagalvao18@gmail.com>
2022-07-21 21:08:21 -0300

    A008 Operadores resolução

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -6,10 +6,10 @@ c) negação de vinte é maior que cinquenta
 
 d) negação de (vinte é maior que cinquenta) */
 
-let operacao1 = (5 > 20 && 5 < 2)
-let operacao2 = (5 == 5 || 5 == "5")
-let operacao3 = (-20 > 50)
-let operacao4 = (-20 > 50)
+let operacao1 = 5 > 20 && 5 < 2
+let operacao2 = 5 == 5 || 5 == "5"
+let operacao3 = !20 > 50
+let operacao4 = !(20 > 50)
 console.log(operacao1)
 console.log(operacao2)
 console.log(operacao3)
