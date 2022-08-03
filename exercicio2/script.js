ae72b96375ef6008b2ae157ec83ed3b9e00b64d3
Elenilson <elenilsonsantos9@gmail.com>
2022-08-02 22:18:30 -0300

    exercicios resolvidos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -3,5 +3,5 @@ const segundoNumero = prompt('digite o segundo numero');
 
 console.log('o primeiro numero é maior que o segundo?', +primeiroNumero > +segundoNumero);
 console.log('o primeiro numero é igual ao segundo?', +primeiroNumero === +segundoNumero);
-console.log('o primeiro numero é divisível pelo o segundo?', +primeiroNumero % +segundoNumero);
-console.log('o segundo numero é divisível pelo o primeiro?', +segundoNumero % +primeiroNumero)
+console.log('o primeiro numero é divisível pelo o segundo?', +primeiroNumero % +segundoNumero ===0);
+console.log('o segundo numero é divisível pelo o primeiro?', +segundoNumero % +primeiroNumero ===0);
