86c5119067bfa98980893db9e65f3638c2c26b41
jeova paiva <jeovappaiva@gmail.com>
2022-08-07 20:05:10 -0300

    a008-operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let numeroPar = +prompt("Digite um número par!");
+
+    if(numeroPar % 2 === 0){ 
+        console.log("par");
+    } else{
+        console.log("impar"); 
+    }
+
+
+
+         
+    
