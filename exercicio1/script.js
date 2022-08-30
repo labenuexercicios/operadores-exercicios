f3ad46a14aab29b7325dd578e5daede5460b5725
Gustavo Lopes Nomelini <gustavolnomelini@gmail.com>
2022-08-30 19:24:35 -0300

    solved exercise 1

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+let number = Number(prompt("Insira um número par:"))
+console.log((number%2))
+//se o número for par o resto da divisão será sempre 0
+//se o número for ímpar o resto da divisão será sempre 1
