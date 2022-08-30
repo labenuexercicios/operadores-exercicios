501df826d079c5ee11d9b0107310ed0efc7b1e64
Mjfreedom <mbuzina@gmail.com>
2022-08-30 12:09:53 -0300

    Exercicio 1 - Antes da Aula

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+//a)
+let par = Number(prompt("Insira o número par?"))
+console.log(`Número par escolhido: ${par}`)
+
+//b)
+console.log(`
+O Resto da divisão é ${par % 2}
+`)
+
+//c)
+//o resto da divisão é sempre 0
+
+//d)
+//o resto da divisão sempre da 1
