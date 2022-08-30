61dbe4f5540f18073b88b0b98854044d5e721537
guirozmesquita@gmail.com <guilhermer_mesquita@hotmail.com>
2022-08-30 13:52:13 -0400

    indentação organizada

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -4,8 +4,8 @@ console.log(operacao1)
 const operacao2 = 5 == 5 || 5 == '5'
 console.log(operacao2)
 
-const operacao3 = !(20>50)
+const operacao3 = !(20 > 50)
 console.log(operacao3)
 
-const operacao4 = !(20 > 50 || 50>70)
+const operacao4 = !(20 > 50 || 50 > 70)
 console.log(operacao4)
