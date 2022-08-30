d2507f9d55c391fbdf0394b153455a03806ef221
marllonrp <marllonrp@yahoo.com.br>
2022-08-30 14:03:15 -0300

    Exercícios finalizados

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let numeroUsuario = prompt  ("Insira um número par:")
+numeroUsuario = Number(numeroUsuario)
+
+let divisao = numeroUsuario%2
+
+console.log(divisao)
+// por ser um número par divido por 2 o restp é sempre zero. 
+// quando se insere um número impar, o resto sempre é 1. 
+
+
