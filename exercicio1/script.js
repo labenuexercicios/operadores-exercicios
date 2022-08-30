f6dc545be7b9783cf5836d6c3fa23d991b8a6a7e
Paula <paulajardimf@gmail.com>
2022-08-30 11:37:37 -0300

    exercício 1 finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let numeroPar = prompt("Insira um número par");
+
+let resultado = Number(numeroPar) % 2;
+
+console.log(Number(resultado));
+
+// c) quando utilizamos o resto da divisão em um número par, a resposta é sempre 0.
+
+// d) quando utilizamos o resto da divisão em um número ímpar, a resposta é sempre 1.
