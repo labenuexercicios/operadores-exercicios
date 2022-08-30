bb86e4e7992ff6527bb7f8984aa5068b6154c643
KellyVanessa2022 <kellyvanessa2017@gmail.com>
2022-08-30 16:31:02 -0300

    exercicio concluido

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+let numero = Number(prompt("digite um numero par"))
+let divisao = numero % 2
+
+console.log  ("o resto da divisão?" , divisao)
---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+const numero1 = Number(prompt("Digite um numero qualquer"))
+const numero2 = Number(prompt("Digite um numero qualquer"))
+
+// 
+
+console.log("o primeiro numero é maior que o segundo?", numero1 > numero2)
+console.log("o primeiro numero é igual ao segundo?", numero1 === numero2)
+console.log("o primeiro numero é divisivel pelo segundo?", numero1 % numero2 === 0)
+console.log("O segundo numero é divisível pelo primeiro?", numero2 % numero1 === 0) 
