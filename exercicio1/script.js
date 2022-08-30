47a7ba6c4fea7d77432a357e5eb6ce524fc32df3
MarianaMeijon <marianameijon1989@gmail.com>
2022-08-30 14:01:56 -0300

    Exercicio 1 pronto

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const numeropar = Number ( prompt ("Digite um número par: "))
+console.log (numeropar)
+
+console.log (numeropar%2)
+
+// padrão notado é que a operação devolve sempre resultado zero, porque número par dividido por dois dará, 
+//, sempre resultado inteiro e portanto resto 0.
+//Caso seja inserido um número ímpar o resto terá um valor diferente de zero, pois um número ímpar
+//,divido por dois não será exato e sempre terá resto.
