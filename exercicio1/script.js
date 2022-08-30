dca6a228e1df2a7bdb649324848b7bb04ddef46f
JayGovinda13 <jaygovinda13@gmail.com>
2022-08-30 15:37:09 -0300

    Pronto

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+// Peça um numero par
+
+let par = prompt("Coloque um numero par")
+console.log(par % 2)
+
+/* c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+R: Sempre o resto é 0 */
+
+/* d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código
+R: Sempre o resto é 1 */
