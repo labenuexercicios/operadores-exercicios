08995b2c998f2b69c57fedb9024ec673a4d68671
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-08-30 12:05:49 -0300

    exercicio 2 feito

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let numeroA= Number(prompt("Escolha um numero"))
+let numeroB= Number(prompt("Escolha outro numero"))
+console.log(numeroA, numeroB)
+console.log("O primeiro numero é maior que segundo?", numeroA>numeroB)
+console.log("O primeiro numero é igual ao segundo?", numeroA===numeroB)
+console.log("O primeiro numero é divisível pelo segundo?", (numeroA%numeroB)===0)
+console.log("O segundo numero é divisível pelo primeiro?", (numeroB%numeroA)===0)
