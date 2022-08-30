59d2eea24429f3a15475ec4a9fa5d86a5dad96f7
jandyra <jandyrapearcebello@gmail.com>
2022-08-30 13:57:47 -0300

    nova alteracão

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const numeroPar = Number(prompt("Insira aqui um numero de valor par"))
+console.log("Seu numero par é:", numeroPar)
+
+let resto = numeroPar % 2
+console.log("Resto do divisão do numero por 2 é:", resto)
+
