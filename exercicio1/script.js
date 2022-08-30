ba3c5d79eeabc337f2c448f8804572390552ebf5
JP-Becker <jpbecker20@gmail.com>
2022-08-30 16:31:07 -0300

    ExercicioA008-João-Becker

---
+++ exercicio1/script.js
@@ -0,0 +1,3 @@
+const numeroPar = Number(prompt("Insira um número par:"))
+console.log (typeof(numeroPar))
+Number (console.log (numeroPar % 2))
---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let numero1 = prompt("Escolha um numero")
+let numero2 = prompt("Escolha outro numero")
+
+console.log("O primeiro numero é maior que o segundo?", numero1 > numero2)
+console.log("O primeiro numero é igual ao segundo?", numero1 === numero2)
+console.log("O primeiro numero é divisível pelo)segundo?",numero1 % numero2 === 0)
+console.log("O segundo numero é divisível pelo primeiro?", numero2 % numero1 === 0)
