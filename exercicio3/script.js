740316ab188ffa182368e767a9c112234fc1005b
Mjfreedom <mbuzina@gmail.com>
2022-08-30 13:48:25 -0300

    Exercício 3 antes da aula

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+//a)
+console.log(`
+5 é maior que 20 e também é menos que 2: ${5 > 20 && 5 < 2}
+5 é igual a 5 ou é igual a "5": ${5 === 5 || 5 === "5"}
+Negação de 20 é maior que 50: ${!20 > 50}
+Negação de 20 é maior que 50 ou 50 é maior que 60: ${!20 > 50 || !50 > 60}
+`)
