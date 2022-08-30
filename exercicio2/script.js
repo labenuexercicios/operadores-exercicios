f5856984cc788063df443c44c70fdaa127c49275
dgcezar <diego.cezar13@gmail.com>
2022-08-30 12:26:47 -0300

    Att1 - exercício 2 faltando apenas o último item

---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+const primeiroNumero = prompt("Insira o primeiro número: ");
+const converterPrimeiroNumero = Number(primeiroNumero);
+
+const segundoNumero = prompt("Insira o segundo número: ");
+const converterSegundoNumero = Number(segundoNumero);
+
+console.log(converterPrimeiroNumero > converterSegundoNumero);
+console.log(converterPrimeiroNumero === converterSegundoNumero);
+console.log(converterPrimeiroNumero % converterSegundoNumero === 0);
+console.log(converterSegundoNumero % converterPrimeiroNumero === 0);
