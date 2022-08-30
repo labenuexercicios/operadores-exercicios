e43d56a524b681a93a570b84c15231aad947afc6
Paula <paulajardimf@gmail.com>
2022-08-30 12:22:03 -0300

    exercício 3 finalizado

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+console.log("5 é maior que 20 e também é menor que 2", 5 > 20 && 5 < 2);
+
+console.log("5 é igual a 5 ou é igual a “5”", 5 === 5 || 5 === "5");
+
+console.log("negação de (vinte é maior que cinquenta)", !(20 > 50));
+
+console.log("negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)", !(20 > 50 || 50 > 60));
