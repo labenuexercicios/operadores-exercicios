fa5603d5c31309cd81d7769c6a9bb77f1c17ca06
atilabarbosa <atilacabo@gmail.com>
2022-08-30 17:37:36 -0300

    exercícios dia 30/08

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+// Peça ao usuário que insira um número **par**
+let numPar = prompt("Insira um número par")
+parseInt(numPar)
+
+//Imprima no console **o resto da divisão** desse número por 2
+console.log("O resto da divisão é ", numPar % 2)
+
+/*Teste o programa com diversos números pares. Você notou um padrão?
+O resto da divisão sempre será 0 */
+
+/*O que acontece no console se o usuário inserir um número ímpar?
+O resto sempre será maior q 0*/
