698782f8d7449aa60bf9ced632f28134c81d2c5e
lucasrodriguesg <lucas.rodrigues.pvd@gmail.com>
2022-08-30 12:01:51 -0300

    Resolução dos exercicios

---
+++ exercicio3/script.js
@@ -0,0 +1,15 @@
+//Resposta A:
+const maiorQue = 5 > 20 && 5 < 2
+console.log ("Maior que:", maiorQue)
+
+//Resposta B:
+const igualQue = 5 === 5 && 5 === "5"
+console.log ("Igual que:", igualQue)
+
+//Resposta C:
+const negacao1 = 20 > 50
+console.log("Maior que:", !negacao1)
+
+//Resposta D:
+const negacao2 = 20 > 50 || 50 > 60 
+console.log("Maior que ou maior que:", !negacao2)
