4d66a2cee20c2d7723bc132a22922774934f4d7a
endioliveira <eomedeiros21@gmail.com>
2022-08-30 13:26:01 -0300

    verificando se valores são maior/menor, igual e fazendo a negaçaõ dos valores.

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+const operacoesLogicas1 = 5 > 20 && 5 < 2
+console.log(operacoesLogicas1)
+
+const operacoesLogicas2 = 5 === 5 || 5 === "5"
+console.log(operacoesLogicas2)
+
+const operacoesLogicas3 = !(20 > 50)
+console.log(operacoesLogicas3)
+
+const operacoesLogicas4 = !(20 > 50 || 50 > 60)
+console.log(operacoesLogicas4)
