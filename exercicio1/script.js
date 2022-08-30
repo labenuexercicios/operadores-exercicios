f13e70de382809200a935cfe8f030d457b8a160c
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2022-08-30 15:49:24 -0300

    a008

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("Insira um numero"))
+let b = 2
+console.log(`O resto da divisão do seu numero é: ${a % b}`)
+// padrão é de que sempre dá zero por conta de ser um numero par não existe o resto da divisão.
+ 
+//quando é impar ele mostra o resultado.
+
---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("insira um numero"))
+let b = Number(prompt("insira mais um numero"))
+let c = 0
+console.log(`O primeiro numero é maior do que o segundo? ${a > b} `)
+console.log(`O primeiro numero é igual ao segundo? ${a === b}`)
+console.log(`O primeiro numero é divisivel pelo segundo?  ${(a % b) === c}`)
+console.log(`O segundo numero é divisivel pelo primeiro? ${(b % a) === c }`)
