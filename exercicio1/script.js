2d969492716604d3c786de98b90586853b9aca4a
OitaloDev <italobandeiraviana@gmail.com>
2022-10-19 16:58:34 -0300

    Exercicio realizado 19/10

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+let numeroPar = prompt("Insira um numero Par")
+
+
+console.log(numeroPar%numeroPar/2)
+
+
+/* //ao realizar qual quer divisão por numero par, o resultado
+será 0 */
+
+
+//Ao inserir um numero impar, o resoltado é 0//
+
+
+
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,13 @@
+const conta1 = 5 > 20 && 5 < 2
+console.log(conta1)
+
+
+const conta2 = 5 == 5 || 5 === "5"
+console.log(conta2)
+
+const conta3 = !(20 > 50)
+console.log(conta3)
+
+
+const conta4 = !(20 > 50 || 50 > 60)
+console.log(conta4)
