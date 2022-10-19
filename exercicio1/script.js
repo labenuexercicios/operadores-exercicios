f7738fa61cac7654336df0c264626093978716e4
bartomsilva <bartomsilva@gmail.com>
2022-10-18 22:48:49 -0300

    feat: resolvido exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+
+let meuNumero = 0
+let restoDivisao = 0
+
+meuNumero = Number(prompt("Digite um número par:"))
+restoDivisao = meuNumero%2
+console.log("O resto da divisão entre ",meuNumero, ' e 2 é ',restoDivisao)
+// para os números pares inteiros o resto é sempre zero, para os números impares o
+// resultado nunca é zero assim como para os números com decimais.
