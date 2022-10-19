384faa930edbab7ce1c442d46d623b9581c925a3
LucasBreia <lucas.devfullstack021@gmail.com>
2022-10-19 16:43:46 -0300

    Exercícos concluídoso

---
+++ exercicio3/script.js
@@ -0,0 +1,14 @@
+//EXERCÍCIO 3
+
+let valorA = 5 > 20 && 5 < 2
+console.log("Valor A é: ", valorA);
+
+let valorB = 5 === 5 || 5 === "5"
+console.log("Valor B é: ", valorB);
+
+let valorC = !(20 > 50);
+console.log("Valor C é: ", valorC);
+
+let valorD = !(20 > 50 || 50 > 70);
+console.log("Valor D é: ", valorD);
+
