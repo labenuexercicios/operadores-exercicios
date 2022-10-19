a27d6843cea41c91425889fea81333bea66f229f
bartomsilva <bartomsilva@gmail.com>
2022-10-18 23:10:45 -0300

    feat: resolvido exercicio 3

---
+++ exercicio3/script.js
@@ -0,0 +1,17 @@
+/*
+Escreva as operações lógicas abaixo em formato de código. Imprima no console se o valor da operação é verdadeira ou falsa:
+
+a) 5 é maior que 20 e também é menor que 2;
+
+b) 5 é igual a 5 ou é igual a “5”;
+
+c) negação de (vinte é maior que cinquenta)
+
+d) negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)
+*/
+
+console.log("a)", (5 > 20) && (5 < 2))
+console.log("b)", (5 === 5) || (5 === "5"))
+console.log("c)", !(20 > 50))
+console.log("d)", !((20 > 50) || (50 > 60)))
+
