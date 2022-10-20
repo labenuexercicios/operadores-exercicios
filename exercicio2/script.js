712b67e8a0d4fb621f6fff262a09740f5961d705
Renan <ren4n_oliveira@hotmail.com>
2022-10-19 22:29:22 -0300

    Exercicio2 Feito

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+//Exercício 2
+
+let num = prompt("Digite um número")
+let num2 = prompt("Digite outro número")
+
+
+
+console.log("O Primeiro número é maior que o segundo?", num>num2 )
+console.log("O primeiro número é igual ao segundo", num==num2)
+console.log("O primeiro número é divisível pelo segundo?", (num%num2==0))
+console.log("O primeiro número é divisível pelo segundo?", (num2%num==0))
