314b1131cb92efe414fd91c23bb186a8ff43c6b0
wangunderline <wangwangrian@gmail.com>
2022-10-20 20:39:49 -0300

    exercicios-operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let par = prompt("insira um num par")
+const div = 2
+resposta = Number(par)
+console.log("answer", par / div, par % div)
+// padrão é que só há resultado no resto com números "quebrados"
+//se for inserido um número ímpar, a divisão ocorre da mesma forma, mas o resto muda de 0 para 1, significando que o número não é divisível
