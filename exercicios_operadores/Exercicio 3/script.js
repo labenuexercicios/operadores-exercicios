60034a716d55b50efe2531a623d645174f4d8fc7
Anderson-Schi <jr93@hotmai.com>
2022-10-21 00:18:33 +0100

    Exercicios operadores resolvidos

---
+++ exercicios_operadores/Exercicio 3/script.js
@@ -0,0 +1,19 @@
+// comentario em linha
+
+/* comentario 
+                em 
+                            bloco */
+
+console.log ("HELLO !");
+
+let qA = 5 > 20 && 5 < 2;
+let qB = 5 === 5 || 5 === "5";
+let qC = !(20 > 50);
+let qD = !(20 > 50 || 50 > 60);
+
+console.log(`5 e maior que 20 e tambem e menor que 2 ? : ${qA}`);
+console.log(`5 e igual a 5 ou e igual a "5"? : ${qB}`);
+console.log(`Negacao de (vinte e maior que cinquenta) : ${qC}`);
+console.log(`negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta) : ${qD}`);
+
+
