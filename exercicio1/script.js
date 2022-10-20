e6b48ac5a362c4425c31f16fb5f2eaf0c78e3e00
Renan <ren4n_oliveira@hotmail.com>
2022-10-19 22:08:57 -0300

    Exercicio1 Feito

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+//Exercício a)
+let numero = Number(prompt("Digite um número par"))
+
+//Exercício b)
+
+numero = numero/2
+
+let resto = numero % 2
+
+console.log(resto)
+
+//Exercício c)
+// O resultado sempre dará 0 ou 1 pois o número é divisivel por 2
+
+
+//Exercício d)
+// O resultado muda para 1.5 ou 0.5 dependendo do numero pois o resto do numero não é divisivel por 2
+
