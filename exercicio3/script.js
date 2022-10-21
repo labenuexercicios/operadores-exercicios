ae95ac962efc5bb486ec8cc66917fd0a8921235c
Gleice-Dourado <gleicielensmv@gmail.com>
2022-10-20 21:30:25 -0300

    correção

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,6 @@
 //EXERCICIO 3 - PARTE 1
 
-//a) 5 é maior que 20 e também é menor que 2;
+//a) 5 é maior que 20 e também é menor que 2
 //b) 5 é igual a 5 ou é igual a “5”;
 //c) negação de (vinte é maior que cinquenta)
 //d) negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta) */
