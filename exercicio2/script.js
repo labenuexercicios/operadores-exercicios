a160d301b50127c1143809b946e6d718a6d953bf
socrampontes <114445301+socrampontes@users.noreply.github.com>
2022-10-23 23:14:33 -0300

    exercicio 008 23/10

---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+const numero0 = prompt ("Digite um numero ");
+let numero1 = prompt ("Digite outro numero ");
+
+console.log(numero0 ,  numero1)
+
+console.log(numero0 > numero1)
+console.log(numero0 === numero1)
+console.log(numero0 % numero1 === 0)
+console.log(numero1 % numero0 === 0) 
+
