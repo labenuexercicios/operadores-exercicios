0f7dd6d6ba84e87ab2aad97e75c5d7982c4298f2
deborasils <deati.33.sils@gmail.com>
2022-11-23 00:13:26 -0300

    operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let resposta1 = prompt("insira um numero par")
+console.log(resposta1 % 2)
+
+// % sempre vai me devolver se o numro é par ou impar 
+// se o numero for impar o resto é diferente de 0
