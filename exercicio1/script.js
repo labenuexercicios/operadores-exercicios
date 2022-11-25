e17abe0ff3642ae692e4631d9089a74afaed9ace
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-11-25 15:53:15 -0300

    Resolução exercicio1 operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+let numeroPar
+
+numeroPar = +(prompt("Insira um numero par:"))
+
+rest = numeroPar%2
+console.log(rest/2)
+
+//o resultado com numero par sempre vai imprir o resto que da 0 
+// o resultado com impar sempre vai imprimir o resto do calculo
+//que geralmente é quebrado.
+
+
+//EXERCICIO//
+
+
+//Construa um programa que:
+
+//a) Peça ao usuário que insira um número **par**
+
+//b) Imprima no console **o resto da divisão** desse número por 2.
+
+//c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+
+//d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código
+
+//>💡  **Dica**:   não se esqueça de **converter** as respostas para o tipo número
