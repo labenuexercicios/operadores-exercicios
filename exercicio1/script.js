996a808426a0d16d5dba0059e43e7ca08d6f389d
astron4ut <ctt.astro@gmail.com>
2022-12-07 01:42:46 -0300

    resolição dos exercícios (faltou a ultima questão)

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+let numeroDoUsuario = prompt("Insira um número par");
+console.log(numeroDoUsuario);
+
+numeroDoUsuario = numeroDoUsuario % 2;
+console.log(numeroDoUsuario);
+
+// O padrão de sempre receber números pares é
+// que o valor do resto sempre será 0;
+
+// Se o programa receber um número impar, o módulo
+// sempre retornará 1.
