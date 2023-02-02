bc125dd2ad0e19ae7304bda57319c8343bfbd939
Thissantana <thiagodssantana@gmail.com>
2023-02-01 23:58:25 -0300

    Respostas do exercício 2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,4 +1,14 @@
+//a
 const numeroPar = Number(prompt("Insira um número par"))
 const restoDivisao = numeroPar % 2
+
+//b
 console.log(`O resto da didisão é: ${restoDivisao}`)
 
+//c
+// Esse resultado sempre vai dar zero, pois não existe sobra
+// divisão de números pares por 2.
+
+//d
+// Quando o usuário insere números impares, o console sempre
+// retorna 1, pois é a sobra da conta.
