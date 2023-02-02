25517e191dc2aaa4b6a9bb219daa69143ae9b96a
Henderson Peraza <hendersoneev@gmail.com>
2023-02-02 14:58:43 -0300

    resultado exercio '

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let par = prompt('Insira um número par')
+console.log('O resto da divisão desse número é:', par = par % 2) // 0
+ /* Quando retorna o resultado do resto da divisão de um número par
+ ele retorna 0 e quando é de um número ímpar retorna 1 */ 
+console.log(typeof par)
