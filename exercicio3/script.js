ed893fd120f69ccba8d993b65c5b6174cf9cce53
Tayrone Teixeira <108040192+Tayrone-TF@users.noreply.github.com>
2023-02-02 16:48:16 -0300

    Corrindo arredondadmento

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -35,55 +35,56 @@ const admissao = 2019
 //1-
 let aux = 45.50
 const salarioMaisAux = salario + aux
-console.log(`O salário fixo + comissão: R$${salarioMaisAux}`)
+console.log(`O salário fixo + comissão: R$${salarioMaisAux.toFixed(2)}`)
 
 //2-
 let vendasJan = 5784.50
 const comissaoJan = percentComissao * vendasJan
-console.log(`A comissão de Fulano em janeiro será de R$${comissaoJan}`)
+console.log(`A comissão de Fulano em janeiro será de R$${comissaoJan.toFixed(2)}`)
 
 //3-
 let descInss = 0.05
 const salarioTotalBrutoJan = salario + comissaoJan
 const salarioTotalLiquidoJan = salarioTotalBrutoJan - (descInss * salarioTotalBrutoJan)
-console.log(`Fulano receberá no mês de janeiro o valor de R$${salarioTotalLiquidoJan}`)
+console.log(`Fulano receberá no mês de janeiro o valor de R$${salarioTotalLiquidoJan.toFixed(2)}`)
 
 //4- 
-console.log(`Janeiro: R$${salarioTotalLiquidoJan}`)
+console.log(`Janeiro: R$${salarioTotalLiquidoJan.toFixed(2)}`)
 
 let vendasFev = 3418.41
 const comissaoFev = percentComissao * vendasFev
 const salarioTotalBrutoFev = salario + comissaoFev
 const salarioTotalLiquidoFev = salarioTotalBrutoFev - (descInss * salarioTotalBrutoFev)
-console.log(`Fevereiro: R$${salarioTotalLiquidoFev}`)
+console.log(`Fevereiro: R$${salarioTotalLiquidoFev.toFixed(2)}`)
 
 let vendasMar = 4124.10
 const comissaoMar = percentComissao * vendasMar
 const salarioTotalBrutoMar = salario + comissaoMar
 const salarioTotalLiquidoMar = salarioTotalBrutoMar - (descInss * salarioTotalBrutoMar)
-console.log(`Março: R$${salarioTotalLiquidoMar}`)
+console.log(`Março: R$${salarioTotalLiquidoMar.toFixed(2)}`)
 
 let vendasAbr = 1874.00
 const comissaoAbr = percentComissao * vendasAbr
 const salarioTotalBrutoAbr = salario + comissaoAbr
 const salarioTotalLiquidoAbr = salarioTotalBrutoAbr - (descInss * salarioTotalBrutoAbr)
-console.log(`Abril: R$${salarioTotalLiquidoAbr}`)
+console.log(`Abril: R$${salarioTotalLiquidoAbr.toFixed(2)}`)
 
 let vendasMai = 7000
 const comissaoMai = percentComissao * vendasMai
 const salarioTotalBrutoMai = salario + comissaoMai
 const salarioTotalLiquidoMai = salarioTotalBrutoMai - (descInss * salarioTotalBrutoMai)
-console.log(`Maio: R$${salarioTotalLiquidoMai}`)
+console.log(`Maio: R$${salarioTotalLiquidoMai.toFixed(2)}`)
 
 let vendasJun = 9450
 const comissaoJun = percentComissao * vendasJun
 const salarioTotalBrutoJun = salario + comissaoJun
 const salarioTotalLiquidoJun = salarioTotalBrutoJun - (descInss * salarioTotalBrutoJun)
-console.log(`Junho: R$${salarioTotalLiquidoJun}`)
+console.log(`Junho: R$${salarioTotalLiquidoJun.toFixed(2)}`)
 
 //5-
 const mediaSalarial =((salarioTotalLiquidoJan + aux) + (salarioTotalLiquidoJan + aux) + (salarioTotalLiquidoJan + aux) +
 (salarioTotalLiquidoJan + aux) + (salarioTotalLiquidoJan + aux) +
 (salarioTotalLiquidoJan + aux)) / 6 
 
-console.log(`A média salarial de janeiro a junho foi R$${mediaSalarial}`)
+console.log(`A média salarial de janeiro a junho foi R$${mediaSalarial.toFixed(2)}`)
+
