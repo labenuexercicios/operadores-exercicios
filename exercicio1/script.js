6fa68fcda2131ff6c6e5b536bce37e61e2a9dc3f
Junior Santos <raimundolourival@gmail.com>
2023-02-02 18:10:31 -0300

    Execicio de operadores completo

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let num = Number(prompt("Digite um numero Par"))
+console.log('Resultado: '+ num%2)
+// c) o resto da divisão por dois de todo numero par é igual a 0
+
+// d) Quando o numero é impar o resto da divisão sempre vai ser 1
---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let num = Number(prompt("Digite o primeiro numero"))
+let num2 = Number(prompt("Digite o segundo numero"))
+
+console.log("O primeiro numero é maior que o segundo? "+ (num>num2))
+console.log("O primeiro numero é igual ao segundo? "+ (num===num2))
+console.log("O primeiro numero é divisível pelo segundo? "+ (num%num2===0))
+console.log("O segundo numero é divisível pelo primeiro?"+ (num2%num===0))
