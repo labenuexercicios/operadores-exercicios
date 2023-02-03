2a6c962c504538022d7471e17f0f9e84d4ae2304
gustaMACHO <henrique.gusta777@gmail.com>
2023-02-02 23:45:08 -0300

    exercicios-operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+// EXERCICIO 1
+
+//a) Peça ao usuário que insira um número **par**
+let numeroUsuario = Number(prompt('Por favor, insira um número par:'))
+
+
+//b) Imprima no console **o resto da divisão** desse número por 2.
+let restoDiv = numeroUsuario % 2
+
+console.log(`o resultado do resto da divisão é: ${restoDiv}`)
+
+/*c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+
+apos fazer o teste com varios numeros pares pude reparar que o valor 
+que o console nos retorna é sempre zero 
+*/
+
+
+/*d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código.
+
+ao inserir um numero impar o programa nos retorna o valor do resto da divisao = 1
+*/
+
+
+
+
