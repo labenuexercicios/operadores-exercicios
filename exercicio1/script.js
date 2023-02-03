0ebe7c197c3d8cdfe6e67ad1166f6999541ae80c
edgargds <joose123edgar@gmail.com>
2023-02-03 19:58:37 -0300

    exercicio-comcluido

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+const operacao1 = Number(prompt("coloque um numero par"))
+console.log("numero par ", operacao1)
+
+const operacao2 = operacao1 / 2 
+console.log("dividir por 2 ",operacao2 )
+
+const operacao3 = operacao2  % 2
+console.log("resto da divisão ", operacao3)
+
+//  vai sempre dar zero por que não sobra nada
+//  se eu colocar impar ele vai ter um valor de resto de divisão
+
+
+
