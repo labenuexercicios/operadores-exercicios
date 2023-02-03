51cba48021e5da4983bb0a70970265c40fbea3c5
Re-Santos <renata.prog@gmail.com>
2023-02-03 11:21:52 -0300

    Alterações

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,49 +1,5 @@
-/* Parte 1
-Escreva as operações lógicas abaixo em formato de código. Imprima no console se o valor da operação é verdadeira ou falsa:
 
-a) 5 é maior que 20 e também é menor que 2;
-
-b) 5 é igual a 5 ou é igual a “5”;
-
-c) negação de (vinte é maior que cinquenta)
-
-d) negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)
-
- */
-
-// Respostas parte 1
-//letra 
-
-const letraA = 5 > 20 && 5 < 2
-console.log (`Letra A: ${letraA}`)
-
-const letraB = (5 === 5) || (5 === "5")
-console.log (`letra B: ${letraB}`)
-
-const letraC = !(20>50)
-console.log (`Letra C: ${letraC}`)
-
-const letraD = ! (20>50 || 50>60)
-console.log (`Letra D: ${letraD}`)
-
-/*
-### Parte 2
-a) Auxílio creche por filho:  R$45,50
-
-b) Comissão de 10% sobre o total de vendas mensal
-
-c) Total de vendas dos meses de janeiro a junho:
-
-    - Janeiro: R$ 5.784,50
-    - Fevereiro: R$ 3.418,41
-    - Março: R$ 4.124,10
-    - Abril: R$ 1.874,00
-    - Maio: R$ 7.000,00
-    - Junho: R$ 9.450,00
-
-d)  Desconto do INSS 5% do salário
-*/
-
+// Parte 1
 const salario= 2000.00
 const creche = 45.50
 const comissao = 0.1 
@@ -54,14 +10,7 @@ const vabr = 1874.00
 const vmai = 7000.00
 const vjun = 9450.00
 
-/*
-1) O salário fixo mais o auxílio creche
-2) Quanto Fulano de Silva receberá de comissão em janeiro ( dica: para calcular 20% de algum valor fazemos o cálculo: valor*0.2)
-3) Quanto Fulano de Silva será descontado em janeiro pelo INSS ( o valor a ser calculado deve levar em consideração o salário + porcentagem de vendas.)
-4) Imprima no console o cálculo do salário de todos os meses com acréscimos e descontos.
-5) A média do salário em  seis meses (de janeiro a junho), levando em consideração as comissões e auxílio. 
-  ( Dica: a média aritmética é calculada somando um grupo de números e dividindo pela quantidade de elementos  somados. Por exemplo, a soma de 2, 3, 3, 5, 7 e 10 é 30 dividido por 6, dará média 5.)
-*/
+// Parte 2
 
 // Respostas
  const salarioauxilio = salario + (2*creche)
