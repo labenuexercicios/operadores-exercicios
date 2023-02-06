7bab60bde24dd688607a8ee30c83bc274321ef50
keeduardo607 <keeduardo607@gmail.com>
2023-02-06 19:19:01 -0300

    Exercicios de Casa, Operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let numeroPar = prompt('Insira um número par:')
+let resto = numeroPar % 2
+
+console.log(`O resto desse cálculo, é: ${resto}`)
+
+// O resto de qualquer número par dividido por 2, é 0.
+// O resto de qualquer número impar dividido por 2, é 1.
