ed9f861a5d7c2bdfd1080622e57d080ace5fa51d
Juliano Amaral <jaygovinda13@gmail.com>
2023-02-07 22:55:18 -0300

    revisão de operadores logicos feita

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Insira um numero par"))
+resto = numero % 2
+
+console.log(resto)
+
+// C - Todos os resultados são iguais a 0 
+// D - Todos os resultados são iguals a 1
