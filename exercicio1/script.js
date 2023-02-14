afaf516585c4b22e9d10fc83041194bba1094da3
LivanDoVale <livanjr4@gmail.com>
2023-02-14 09:42:27 -0300

    exercicio 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let num = "Insira um numero par"
+let respNum = Number(prompt(num))
+
+let divisao = Number(respNum % 2)
+
+console.log(`o resto da divisão do ${respNum} é: ${divisao} `)
+
+// C) O resto sempre da 0
+// D) O resto sempre da 1
