3d99a0c6ad768a1711506870afb3f579a74ae2ed
bandreid01 <viniciusdasilvax@gmail.com>
2023-03-15 15:31:41 -0300

    Resolução Exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+n1 = Number(prompt("Digite o Primeiro Número: "))
+n2 = Number(prompt("Digite o Segundo Número: "))
+
+
+console.log("O Primeiro Número é maior que o Segundo: ",n1>n2)
+console.log("O Primeiro Número é igual ao Segundo: ",n1===n2)
+console.log("O Primeiro Número é divisível pelo Segundo: ",(n1%n2===0))
+console.log("O Segundo Número é divisível pelo Primeiro: ",(n2%n1===0))
