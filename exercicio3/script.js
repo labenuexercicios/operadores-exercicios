e8903c2eaba73f3fb8886289615421af5ff9f347
bandreid01 <viniciusdasilvax@gmail.com>
2023-03-15 16:39:56 -0300

    Resolução Exercício 3

---
+++ exercicio3/script.js
@@ -0,0 +1,20 @@
+// let conta = 5 > 20 && 5 < 2
+// console.log(conta)
+
+// console.log(5===5 || "5")
+// console.log(20 > !50)
+// console.log(20 > !50 || !50 > 60)
+
+const nome = "Fulano da Silva"
+let filhoes = 2
+let salario = 2000
+let comissao = 0.1
+let admissao = 2019
+let auxilio = 45.50
+let janeiro = (salario + (5784.50 * 0.2))
+
+// console.log(salario)
+console.log("Salário fixo + auxilio: ", salario + (filhoes * auxilio), "R$")
+console.log("Comissão em Janeirio ", 5784.50 * 0.2 , "R$")
+console.log("Desconto em Janeiro ", janeiro * 0.05 , "R$")
+
