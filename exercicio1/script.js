a0aaf43947dd6630f2a19a96c1f31ddea13c359c
caroole01~ <caroline_voleck@hotmail.com>
2023-03-16 12:36:45 -0300

    feats

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let user = (prompt("Digite um numero par!"));
+let rest = user % 2
+//console.log(rest)
+//Quando realiza a operação, ele imprime no console o resto da divisão que sobra por 2 quando setado numero impar.
+//Quando inserido numero impar, retorna o resto da divisão, e numero par retorna 0.
---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let numberOne = prompt("Digite um numero");
+let  numberTwo = prompt("Digite outro numero");
+let big = numberOne > numberTwo
+    console.log(big)
+
+let equal = numberOne === numberTwo
+    console.log(equal)
+
+let division = numberOne % numberTwo == 0
+    console.log(division)
+
+let divisionConvertion = numberTwo % numberOne == 0
+    console.log(divisionConvertion)    
