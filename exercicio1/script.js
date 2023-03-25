86aec846ec695a5209a23db97f366d8888ebcb3c
Orcisantos76 <orcisantos76@gmail.com>
2023-03-25 15:42:37 -0300

    refazendo exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,17 +1,14 @@
 // # Exercício 1 envio
-
+console.log(`Rodou`)
 // Construa um programa que:
 
 // a) Peça ao usuário que insira um número **par**
-console.log("ola")
-const num = Number(prompt("Digite um numero par:"))
+const n1 = parseint(prompt(`Insira um número: `))
 // b) Imprima no console **o resto da divisão** desse número por 2.
-const resultado = num%2
-console.log(`O resto de ${num} dividido por 2 é ${resultado}`)
-console.log(`O tipo do primeiro numero é ${typeof(num)} porque eu ja dei o num/prompt, e o tipo do resultado eu nao alterei entao é ${typeof(resultado)}`)
+console.log(`${n1%2}`)
 
 // c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
 // o resultado sempre é o mesmo
 // d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código.
-// o resultado para impar é 1 porque sempre resta 1 quan
+// o resultado para impar é 1 porque sempre resta 1 quando é impar o reto é 1
 // >💡  **Dica**:   não se esqueça de **converter** as respostas para o tipo número
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -3,17 +3,19 @@ console.log("orci")
 // # Exercício 2 envio
 
 // Faça um programa que pergunte ao usuário dois números. Em seguida, faça as operações e imprima no console as seguintes mensagens seguidas pelo `true` ou `false`:
-const n1 = Number(prompt("digite o primeiro numero"))
-const n2 = Number(prompt('Digite o segundo numero'))
+const n1 = Number(prompt(`Insira um número:`))
+const n2 =Number(prompt(`Insira um segundo número:`))
 // ```
 // O primeiro numero é maior que o segundo? true
-console.log(`O primeiro numero, que é ${n1}, é maior que o segundo número que é ${n2} a resposta é ${n1>n2}`)
+console.log(`O primeiro número é ${n1} e o segundo é ${n2}, a resposta é ${n1>n2}`)
+
 // O primeiro numero é igual ao segundo? false
-console.log(`O primeiro numero que é ${n1}, é igual ao segundo número que é ${n2} a resposta é ${n1===n2}`)
+console.log(`O primeiro número é igual ao segundo? ${n1===n2} === ,${n1==n2}==`)
 // O primeiro numero é divisível pelo segundo? true
-console.log(`O primeiro numero que é ${n1}, é divisivel pelo segundo número que é ${n2} a resposta é ${n1%n2}`)
+console.log(`O primeiro número é divisivel pelo segundo ${n1%n2===0}`)
 // O segundo numero é divisível pelo primeiro? true
-console.log(`O segundo numero que é ${n2}, é divisivel pelo primeiro número que é ${n1} a resposta é ${n2%n1 ===0}`)
+console.log(`O segundo número é divisivel pelo primeiro? ${n2%n1===0}`)
+
 
 // divisível = com resto zero
 
