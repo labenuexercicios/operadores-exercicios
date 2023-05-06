6bbfc8463d12dba23d2e4e2ff206a29b627d2e9e
Daylton Araujo <dayltongg46@gmail.com>
2023-05-05 21:26:50 -0300

    exercicio1

---
+++ exercicio2/script.js
@@ -0,0 +1,23 @@
+let numero1= Number(prompt("Insira um numero"));
+let numero2= Number(prompt("Insira outro numero"));
+
+console.log("O primeiro numero é maior que o segundo?", numero1 > numero2);
+console.log("O primeiro numero é igual ao segundo?", numero1 === numero2);
+console.log("O primeiro numero é divisível pelo segundo?", numero1 % numero2 === 0);
+console.log("O segundonumero é divisível pelo primeiro?", numero2 % numero1 === 0);
+
+
+//# Exercício 2
+
+//Faça um programa que pergunte ao usuário dois números. Em seguida, faça as operações e imprima no console as seguintes mensagens seguidas pelo `true` ou `false`:
+
+//```
+//O primeiro numero é maior que o segundo? true
+//O primeiro numero é igual ao segundo? false
+//O primeiro numero é divisível pelo segundo? true
+//O segundo numero é divisível pelo primeiro? true
+
+//divisível = com resto zero
+
+//Obs: A resposta (true ou false) vai depender dos números inseridos e do resultado das operações. As respostas colocadas acima são apenas exemplos!
+//```
