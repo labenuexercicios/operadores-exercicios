b6ff61d278413ea12e6cf795370238711801db57
Daylton Araujo <dayltongg46@gmail.com>
2023-05-05 21:27:57 -0300

    exercicio2

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Insira um numero par:"))
+
+console.log(numero % 2)
+
+//Quando inserimos um numero par, o resto é sempre 0,
+//quando um numero impar é inserido, o resto nunca é 0. esse é o padrão.
