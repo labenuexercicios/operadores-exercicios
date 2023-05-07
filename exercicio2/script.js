e309c27b3238866f392e79c2c6041cc545c63265
Daniel Weslley Silva Xavier <danielweslley882@gmail.com>
2023-05-07 01:03:17 -0300

    Operados-exercicio

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+let numero = Number(prompt('Digite um número')) ; 
+
+let numero2 = Number(prompt('Digite um número'));
+
+
+console.log(`O primeiro número é maior que o segundo? ${numero > numero2}`);
+
+console.log(`O primeiro número é igual ao segundo?  ${numero === numero2}`);
+
+console.log(`O primeiro número é divisivel pelo o segundo?  ${numero % numero2===0} `);
+
+console.log(`O segundo número é divisivel pelo o primeiro?  ${numero2 % numero===0} `);
+
+/* Um número é divisível por outro quando o resto da divisão entre ambos é igual a zero.
+   Os critérios de divisibilidade são regras para saber, de uma forma rápida, se um número é ou não divisível por outro,
+   isto é, se a divisão será exata.*/
+
