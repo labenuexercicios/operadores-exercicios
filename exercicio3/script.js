2dcec3e7989587123c07ff432372e4db08ee3927
edson <mbgraficarn@gmail.com>
2023-05-06 21:06:41 -0300

    nova-alteracao

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -20,24 +20,49 @@ let comissaoMarc = 4124.10 * 0.10
 let comissaoAbr = 1874 * 0.10
 let comissaoMai = 7000 * 0.10
 let comissaoJun = 9450 * 0.10
+console.log("Comissão de Janeiro R$", comissaoJan);
+console.log("Comissão de Fevereiro R$", comissaoFev);
+console.log("Comissão de Março R$", comissaoMarc);
+console.log("Comissão de Abril R$", comissaoAbr);
+console.log("Comissão de Maio R$", comissaoMai);
+console.log("Comissão de Junho R$", comissaoJun);
+console.log("Total de Comissão de Jan a Junho R$", comissaoJan+comissaoFev+comissaoMarc+comissaoAbr+comissaoMai+comissaoJun);
+//JANEIRO
 let janeiro = salarioFixo + comissaoJan 
 let descontoInssJan = (janeiro * 0.05)
-console.log("Salario + Comissão com desconto INSS de Janeiro R$", janeiro-descontoInssJan)
+console.log ("Salário de Janeiro c/ Comissão R$ ", janeiro)
+console.log ("Desconto INSS Jan! R$", descontoInssJan)
+console.log("Salario com desconto INSS de Janeiro R$", janeiro-descontoInssJan)
+//FEVEREIRO 
 let fereveiro = salarioFixo + comissaoFev
 let descontoInssFev = (fereveiro * 0.05)
-console.log("Salario + Comissão com desconto INSS de Fevereiro R$",fereveiro-descontoInssFev)
+console.log ("Salário de Fevereiro c/ Comissão R$ ", fereveiro)
+console.log ("Desconto INSS Fev! R$", descontoInssFev)
+console.log("Salario com desconto INSS de Fevereiro R$",fereveiro-descontoInssFev)
+//MARÇO
 let marco = salarioFixo + comissaoMarc
 let descontoInssMar = (marco * 0.05)
-console.log("Salario + Comissão com desconto INSS de Março R$",marco-descontoInssMar)
+console.log("Salario de Março c/ Comissão R$",marco)
+console.log("Desconto INSS MARC! R$", descontoInssMar);
+console.log("Salario com Desconto INSS de Março R$", marco-descontoInssMar)
+//ABRIL
 let abril = salarioFixo + comissaoAbr
 let descontoInssAbr = (abril * 0.05)
-console.log("Salario + Comissão com desconto INSS de Abril R$",abril-descontoInssAbr)
+console.log("Salario de Abril c/ Comissão R$",abril)
+console.log("Desconto INSS Abril! R$", descontoInssAbr);
+console.log("Salario com Desconto INSS de Abril R$", abril-descontoInssAbr)
+//MAIO
 let maio = salarioFixo + comissaoMai
 let descontoInssMai = (maio * 0.05)
-console.log("Salario + Comissão com desconto INSS de Maio R$",maio-descontoInssMai)
+console.log("Salario de Maio c/ Comissão R$",maio)
+console.log("Desconto INSS Maio! R$", descontoInssMai);
+console.log("Salario com Desconto INSS de Maio R$", maio-descontoInssMai)
+//JUNHO
 let junho = salarioFixo + comissaoJun
 let descontoInssJun = (junho * 0.05)
-console.log("Salario + Comissão com desconto INSS de Junho R$",junho-descontoInssJun)
+console.log("Salario de Junho c/ Comissão R$",junho)
+console.log("Desconto INSS Junho! R$", descontoInssJun);
+console.log("Salario com Desconto INSS de Junho R$", junho-descontoInssJun)
 
 
 let mediaSalarial = (janeiro+fereveiro+marco+abril+maio+junho) / 6
