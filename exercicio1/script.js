05554d8d5da3b3fa90358ed36090939594769eac
Roberta2410 <138620335+Roberta2410@users.noreply.github.com>
2023-07-14 16:02:29 -0300

    exercicio de 5 horas para conclusão

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+//Peça ao usuário inserir um numero par
+
+let num1 = +prompt('Insira um numero par');
+let num2 = 6
+console.log(num1%2)
+//consta o numero restante
+
+console.log (typeof num1)
+
+
+
+
+
