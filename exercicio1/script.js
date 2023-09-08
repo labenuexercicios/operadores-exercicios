f511bbf1fa7473b0ba9c9423b82cfbf622c1a264
Thiago Rosa <thiagorcon@hotmail.com>
2023-09-08 13:33:15 -0300

    envio exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let  num = +prompt('Digite um número')
+numRes = num % 2
+console.log(`O resto da dvisão deste numero é ${numRes}`);
+// Está sendo impresso o valor restante. Quando o número é par imprime zero 
+// Quando é informado um número e ímpar imprime 1.
+
