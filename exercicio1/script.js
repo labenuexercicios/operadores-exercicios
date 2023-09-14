2382adea42969e1035a20ad237ad93061abbb50f
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-14 10:23:51 -0500

    exercicio1-feito

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let par = prompt('Insira um numero par')
+let resto = par / 2
+console.log(`o resto da divisao de ${par} por 2 é ${resto}`);
+
+//C RESPOSTA
+// O padrao que eu vejo é que divide por 2
+
+// D RESPOSTA
+// inserindo um numero impar ainda sim ele divide por 2
